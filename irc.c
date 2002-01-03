368877d7d7c3b61904f7c1b75cef5d06e1cf77b5
strtok <strtok@blitzed.org>
2002-01-03 05:06:00 +0000

    Create socket / Connect to IRC server (Primitive, no error checking)

--- irc.c
+++ irc.c
@@ -21,11 +21,52 @@ along with this program; if not, write to the Free Software
 */
 
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "irc.h"
 #include "config.h"
+#include "extern.h"
+
+int IRC_FD = -1;              /* File descriptor for IRC client */
+struct sockaddr_in IRC_SVR;
+
 
 void irc_cycle()
 {
+      /* No socket open */
+	
+      if(IRC_FD == -1)
+	{
+             memset(&IRC_SVR, 0, sizeof(IRC_SVR));
+	     
+	     IRC_SVR.sin_family = AF_INET;
+	     IRC_SVR.sin_port = CONF_PORT;
+             IRC_SVR.sin_addr.s_addr = inet_addr(CONF_SERVER); 
+             
+	     if(IRC_SVR.sin_addr.s_addr == INADDR_NONE)
+		  return;   /* Generate ERROR here */
 
+             /* Request File Descriptor for Socket */
+	     
+	     IRC_FD = socket(PF_INET, SOCK_STREAM);
+
+	     if(IRC_FD == -1)
+		  return;  /* Generate ERROR here */
+
+	     /* Connect to IRC Server */
+
+	     if(connect(IRC_FD, &IRC_SVR, sizeof(IRC_SVR)) == -1)
+		  return; /* Generate ERROR here */
+	
+	}
 }
+
+
