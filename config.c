6970fdabb75e63a58ab4912090296cd8c7862ad3
andy <andy@blitzed.org>
2002-04-29 14:15:59 +0000

    config.c: 	Tidied up indentation.
    	Added static linkage for all private config functions.
    
    	Added add_to_list() to simplify adding values to configuration
    	lists.
    
    	Added free_list() to simplify clearing out each config list at
    	startup/rehash.
    
    	Added add_to_config() to simplify adding a key/value pair to our
    	config hash.
    
    config.h:
    	Neater indentation, some prototypes moved to config.c.

--- config.c
+++ config.c
@@ -36,6 +36,12 @@ along with this program; if not, write to the Free Software
 #include "extern.h"
 #include "match.h"
 
+static void config_checkreq(void);
+static void config_memfail(void);
+static void add_to_list(string_list *oldlist, const char *item);
+static void free_list(string_list *list);
+static void add_to_config(const char *key, const char *val);
+
 /* Global Configuration Variables */
 
 char *CONF_SERVER          = 0;
@@ -102,167 +108,176 @@ config_hash hash[] = {
 
 void config_load(char *filename)
 {
-    FILE *in;
+	/* 1k buffer for reading the file. */
+	char line[1024];
+	size_t i;
+	char *key, *args;
+	FILE *in;
 
-    char line[1024];  /* 1k buffer for reading the file */
-
-    char *key;
-    char *args;
-
-    string_list *list, *oldlist, *nextlist;
-
-    size_t i;
-
-    if(!(in = fopen(filename, "r")))
-     {
-	log("CONFIG -> No config file found, aborting.");
-	exit(1);
-     }
+	if (!(in = fopen(filename, "r"))) {
+		log("CONFIG -> No config file found, aborting.");
+		exit(1);
+	}
     
-    /* Clear anything we have already */
-    for(i = 0; i < (sizeof(hash) / sizeof(config_hash) - 1); i++)
-      {
-         switch(hash[i].type)
-          { 
-              case TYPE_STRING:
-                  if(( *(char**) hash[i].var))
-                       free(*(char**)hash[i].var);
-                  *(char**)hash[i].var = 0;
-                  break;
-              case TYPE_INT:
-                  *(int *) hash[i].var = 0;
-		  break;
-              case TYPE_LIST:
-                  for(list = * (string_list **) hash[i].var;list;)
-                   {
-                       nextlist = list->next;
-                       free(list->text);
-                       free(list);
-                       list = nextlist;
-                   }
-                   list = 0;
-                   *(string_list **) hash[i].var = 0;
- 
-                   break;
-          }
-         hash[i].reqmet = 0;
-      }
+	/* Clear anything we have already. */
+	for (i = 0; i < (sizeof(hash) / sizeof(config_hash) - 1); i++) {
+		switch(hash[i].type) { 
+		case TYPE_STRING:
+			if (( *(char**) hash[i].var))
+				free(*(char**)hash[i].var);
+			*(char**)hash[i].var = 0;
+			break;
+		case TYPE_INT:
+			*(int *) hash[i].var = 0;
+			break;
+		case TYPE_LIST:
+			free_list(* (string_list **) hash[i].var);
+			break;
+		}
+		hash[i].reqmet = 0;
+	}
 
-    while(fgets(line,1023, in))  
-      {
-
-	    if(line[0] == '#')
-		 continue;
+	while (fgets(line, 1023, in))  {
+		if(line[0] == '#')
+			continue;
 		    
-            key = strtok(line, " ");
-            args = strtok(NULL, "\n");
+		key = strtok(line, " ");
+		args = strtok(NULL, "\n");
 
-            if(!args)
-               continue;
+		if (!args)
+			continue;
 
+		/* Strip leading and trailing spaces. */
+		args = clean(args);
+		add_to_config(key, args);
+	}
 
-            args = clean(args); /* Strip leading and tailing spaces */
-
-            for(i = 0; i < (sizeof(hash) / sizeof(config_hash)) - 1; i++)
-              if(!strcasecmp(key, hash[i].key))
-                {
-                      switch(hash[i].type)
-                        {
-                            case TYPE_STRING: 
-                                 *(char**) hash[i].var = strdup(args);
-                                 break;
-                            case TYPE_INT:
-                                 *(int *) hash[i].var = atoi(args);
-                                 break;
-			    case TYPE_LIST:
-				 list = malloc(sizeof(string_list));
-		                 if(!list)
-				    config_memfail();
-				 list->next = NULL;
-				 list->text = strdup(args);
-				 if(!list->text)
-				    config_memfail();
-				 collapse(list->text);
-				 oldlist = * (string_list **) (hash[i].var);
-				 if(oldlist)
-				  {
-				    while(oldlist)
-				     {
-				       if(strcasecmp(args, oldlist->text))
-					{
-					  if(oldlist->next)
-					     oldlist = oldlist->next;
-					  else
-					   {
-					     oldlist->next = list;
-					     oldlist = NULL;
-					   }
-					}
-				       else
-					{
-					  free(list);
-					  oldlist = NULL;
-					}
-				     }
-				  }
-				 else
-				  {
-				    * (string_list **) (hash[i].var) = list;
-				  }
-				break;
-                        }
-                       hash[i].reqmet = 1;
-                }
-      }
-
-  fclose(in);
-  config_checkreq(); /* Check required parameters */
+	fclose(in);
+	/* Check required parameters. */
+	config_checkreq();
 }
 
-void config_checkreq()
+static void config_checkreq()
 {
-      size_t i;
-      int errfnd = 0;
-      string_list *list;
+	size_t i;
+	int errfnd;
+	string_list *list;
 
-      for(i = 0; i < (sizeof(hash) / sizeof(config_hash) - 1); i++)
-        if(hash[i].req && !hash[i].reqmet)
-         {               
-            log("CONFIG -> Parameter [%s] required but not defined in config.", hash[i].key);
-            errfnd++;
-         }
-        else if(OPT_DEBUG >= 3 && hash[i].reqmet)
-         {
-            switch(hash[i].type)
-             {
-                 case TYPE_STRING:
-                      log("CONFIG -> Set [%s]: %s", hash[i].key, *(char**) hash[i].var);
-                      break;
-                 case TYPE_INT:
-                      log("CONFIG -> Set [%s]: %d", hash[i].key, *(int *) hash[i].var);
-                      break;
-                 case TYPE_LIST:
-                      for(list = *(string_list**) (hash[i].var); list; list = list->next)
-                         log("CONFIG -> Set [%s]: %s", hash[i].key, list->text);
-                      break;
-             }
-         }
+	errfnd = 0;
 
+	for (i = 0; i < (sizeof(hash) / sizeof(config_hash) - 1); i++) {
+		if (hash[i].req && !hash[i].reqmet) {               
+			log("CONFIG -> Parameter [%s] required but not "
+			    "defined in config.", hash[i].key);
+			errfnd++;
+		} else if (OPT_DEBUG >= 3 && hash[i].reqmet) {
+			switch (hash[i].type) {
+			case TYPE_STRING:
+				log("CONFIG -> Set [%s]: %s", hash[i].key,
+				    *(char**) hash[i].var);
+				break;
+			case TYPE_INT:
+				log("CONFIG -> Set [%s]: %d", hash[i].key,
+				    *(int *) hash[i].var);
+				break;
+			case TYPE_LIST:
+				for (list = *(string_list**) (hash[i].var);
+				    list; list = list->next) {
+					log("CONFIG -> Set [%s]: %s",
+					    hash[i].key, list->text);
+				}
+				break;
+			}
+		}
+	}
 
-      if(errfnd)
-       {
-          log("CONFIG -> %d parameters missing from config file, aborting.", errfnd);
-          exit(1);
-       }
+	if (errfnd) {
+		log("CONFIG -> %d parameters missing from config file, "
+		    "aborting.", errfnd);
+		exit(1);
+	}
 }
 
-/*  Called when memory allocation somewhere returns
- *  an error
+/*
+ * Called when memory allocation somewhere returns an error.
  */
 
-void config_memfail()
+static void config_memfail(void)
 {
-     log("CONFIG -> Error allocating memory.");
-     exit(1);
+	log("CONFIG -> Error allocating memory.");
+	exit(1);
 }
 
+static void add_to_list(string_list *oldlist, const char *item)
+{
+	string_list *list;
+
+	list = malloc(sizeof(*list));
+	
+	if (!list)
+		config_memfail();
+	
+	list->next = NULL;
+	list->text = strdup(item);
+	
+	if (!list->text)
+		config_memfail();
+	
+	collapse(list->text);
+
+	if (!oldlist) {
+		oldlist = list;
+		return;
+	}
+
+	while (oldlist) {
+		if (strcasecmp(list->text, oldlist->text)) {
+			if (oldlist->next) {
+				oldlist = oldlist->next;
+			} else {
+				oldlist->next = list;
+				oldlist = NULL;
+			}
+		} else {
+			free(list);
+			oldlist = NULL;
+		}
+	}
+}
+
+static void free_list(string_list *list)
+{
+	string_list *t, *nextlist;
+	
+	for (t = list; t; ) {
+		nextlist = t->next;
+		free(t->text);
+		free(t);
+		t = nextlist;
+	}
+	t = 0;
+	list = 0;
+} 
+
+static void add_to_config(const char *key, const char *val)
+{
+	size_t i;
+	
+	for (i = 0; i < (sizeof(hash) / sizeof(config_hash)) - 1; i++) {
+		if (strcasecmp(key, hash[i].key) == 0) {
+			switch (hash[i].type) {
+			case TYPE_STRING: 
+				*(char**) hash[i].var = strdup(val);
+				break;
+			case TYPE_INT:
+				*(int *) hash[i].var = atoi(val);
+				break;
+			case TYPE_LIST:
+				add_to_list(* (string_list **) (hash[i].var),
+				    val);
+				break;
+			}
+			hash[i].reqmet = 1;
+		}
+	}
+}
--- config.h
+++ config.h
@@ -1,33 +1,29 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-      typedef struct config_hash config_hash;
+typedef struct config_hash config_hash;
 
 #define TYPE_STRING 1
 #define TYPE_INT 2
 #define TYPE_LIST 3
 
-      struct config_hash
-       {
-           char *key;
-           int type;
-           int req;     /* Item is required */
-           int reqmet;  /* Req met          */
-           void *var;
-       };
+struct config_hash
+{
+	char *key;
+	int type;
+	int req;     /* Item is required */
+	int reqmet;  /* Req met          */
+	void *var;
+};
 
-      typedef struct string_list string_list;
+typedef struct string_list string_list;
       
-      struct string_list
-       {
-         string_list *next;
-	 char *text;
-       };
+struct string_list
+{
+	string_list *next;
+	char *text;
+};
        	
-       /* Config Functions */
-
-       void config_load(char *filename);
-       void config_checkreq();
-       void config_memfail();
+extern void config_load(char *filename);
 
 #endif
