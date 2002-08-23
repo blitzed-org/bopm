1ef2c0cf82d647ac2120fc84c7381dea8db9811f
andy <andy@blitzed.org>
2002-08-23 05:42:15 +0000

    src/negcache.[ch]: 	Implementation of a patricia trie for storing IP addresses and 	timestamps.  This data structure will allow searches for nodes 	with only log_2 N bit comparisons where N is the current number of 	nodes.  It also only requires as many nodes as there are IP 	addresses to store.
    	Each node stores a key (the IP address), a timestamp, the bit
    	index, and left and right branches.  The bit index is what makes
    	this different from a radix search tree, it tells us at which bit
    	this node's key differs from those above it in the trie.
    
    	Properties of the trie:
    	  1) The bit index always decreases as we follow the tree from the head
    	     to an external node.
    	  2) Each branch of an external node points to the only node that can
    	     contain keys that match the bit pattern.  All searches terminate
                 at external nodes.
    	  3) When trying to search for a bit pattern that is not present in the
    	     tree, you will hit an external node at the place where your bit
    	     pattern first deviates from all current nodes.  You can tell this
    	     has happened because the next node's bit index will be larger than
                 the current, which would be contrary to point (1).
    	  4) Because the bit increments in each node store information about
    	     where each node's bit pattern differs from all others in the tree,
                 extra nodes are not needed - unlike in a radix tree.
    	  5) As for a radix tree, a patricia trie will always end up the
    	     same no matter what order the nodes are inserted.

---
+++ src/negcache.c
@@ -0,0 +1,285 @@
+/*
+Copyright (C) 2002 Andy Smith
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to:
+
+      the Free Software Foundation, Inc.
+      59 Temple Place - Suite 330
+      Boston, MA  02111-1307, USA.
+
+*/
+
+/*
+ * A Negative caching implementation for IPv4 addresses.  The idea is that
+ * every time an IP address is seen, it is checked against a patricia trie.  If
+ * the IP address was previously seen and within an acceptable period of time,
+ * it is not scanned again.  Otherwise, the address is scanned as normal.  If
+ * it is proven to be OK (i.e.  it doesn't run an open proxy) then it is added
+ * to the trie.
+ *
+ * I'm using a patricia trie as described on p253 of Sedgewick.  The reason for
+ * this is that it can find any IP address with only log_2 N bit comparisons
+ * and only requires as many nodes as we have IP addresses to store.
+ *
+ * I would have liked to have made the trie implementation generic but couldn't
+ * see how to do it easily given that the trie structure depends on the data
+ * within it.
+ *  --grifferz
+ */
+
+#include "setup.h"
+
+#include <stdio.h>
+
+#ifdef STDC_HEADERS
+#include <stdlib.h>
+#endif
+
+#ifdef TIME_WITH_SYS_TIME
+#include <sys/time.h>
+#include <time.h>
+#else
+# ifdef HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
+#include "inet.h"
+#include "irc.h"
+#include "negcache.h"
+
+extern unsigned int CONF_NEG_CACHE;
+extern unsigned int OPT_DEBUG;
+
+static struct cnode *nc_search(struct cnode *head, const unsigned long ip);
+static struct cnode *nc_insert(struct cnode *head, const unsigned long ip);
+static void nc_rebuild(struct cnode *old_head, struct cnode *new_head,
+    struct cnode *n, time_t now);
+
+time_t last_nc_expire;
+unsigned int maxb;
+struct cnode *nc_head = NULL;
+
+/*
+ * Return the bit which appears k bits from the right in x.
+ */
+#define GETBIT(x,k)	((x >> k) & 1)
+	
+
+/*
+ * Initialise the patricia trie we use for storing our negative cache.
+ */
+void nc_init(struct cnode **head)
+{
+	if (*head) {
+		/* Cache already exists */
+		return;
+	}
+
+	*head = malloc(sizeof(**head));
+
+	maxb = (sizeof((*head)->ip) * 8);
+	(*head)->ip = 0;
+	(*head)->b = maxb;
+	(*head)->l = (*head)->r = *head;
+	last_nc_expire = time(NULL);
+}
+
+/*
+ * Find and return a pointer to the trie node that corresponds to a given IP
+ * address as expressed in network form, or return NULL if the IP isn't
+ * present.
+ */
+static struct cnode *nc_search(struct cnode *head, const unsigned long ip)
+{
+	struct cnode *p, *x;
+
+	p = head;
+	x = head->l;
+
+	while (p->b > x->b) {
+		p = x;
+		x = GETBIT(ip, x->b) ? x->r : x->l;
+	}
+
+	if (ip == x->ip)
+		return(x);
+	else
+		return(NULL);
+}
+
+/*
+ * Insert a new node into the trie, and return a pointer to it.
+ */
+static struct cnode *nc_insert(struct cnode *head, const unsigned long ip)
+{
+	int i;
+	struct cnode *p, *t, *x;
+
+	i = maxb;
+	p = head;
+	t = head->l;
+
+	while (p->b > t->b) {
+		p = t;
+		t = GETBIT(ip, t->b) ? t->r : t->l;
+	}
+
+	if (ip == t->ip) {
+		/* Node already exists. */
+		return(t);
+	}
+
+	while (GETBIT(t->ip, i) == GETBIT(ip, i))
+		i--;
+
+	p = head;
+	x = head->l;
+
+	while (p->b > x->b && x->b > i) {
+		p = x;
+		x = GETBIT(ip, x->b) ? x->r : x->l;
+	}
+
+	t = malloc(sizeof(*t));
+	t->ip = ip;
+	t->b = i;
+	t->l = GETBIT(ip, t->b) ? x : t;
+	t->r = GETBIT(ip, t->b) ? t : x;
+
+	if (GETBIT(ip, p->b))
+		p->r = t;
+	else
+		p->l = t;
+
+	return(t);
+}
+
+/*
+ * Check whether an IP address is in our negative cache and was added
+ * recently enough.  Return a pointer to its node if so, NULL otherwise.
+ */
+struct cnode *check_neg_cache(const unsigned long ip)
+{
+	time_t now;
+	struct cnode *n;
+
+	if (!CONF_NEG_CACHE)
+		return(NULL);
+
+	n = nc_search(nc_head, ip);
+
+	if (n) {
+		/* Check it is recent enough. */
+		now = time(NULL);
+
+		if (now - n->seen <= CONF_NEG_CACHE)
+			return(n);
+	}
+
+	return(NULL);
+}
+
+/*
+ * Prepare an ASCII string representing an IPv4 address for inserting into
+ * our negative cache.
+ */
+void negcache_insert(const char *ipstr)
+{
+	struct bopm_sockaddr ip;
+	struct cnode *n;
+
+	if (!inet_pton(AF_INET, ipstr, &(ip.sas.sa4.sin_addr))) {
+		log("Invalid IPv4 address '%s'", ipstr);
+		return;
+	} else if (OPT_DEBUG >= 1) {
+		log("All scans against '%s' have failed, inserting "
+		    "negative cache entry", ipstr);
+	}
+
+	n = nc_insert(nc_head, ip.sas.sa4.sin_addr.s_addr);
+
+	if (n)
+		n->seen = time(NULL);
+}
+
+/*
+ * Recursively walk the cache and insert the nodes into a new patricia trie,
+ * skipping nodes that are too old.
+ */
+static void nc_rebuild(struct cnode *old_head, struct cnode *new_head,
+    struct cnode *n, time_t now)
+{
+	struct cnode *new;
+
+	if (!n) {
+		/* Start at head. */
+		n = old_head->l;
+	}
+
+	if (n == old_head) {
+		/* Trie is empty. */
+		return;
+	}
+
+	if (n->b > n->l->b) {
+		/*
+		 * If the trie extends via the left branch, follow it
+		 * recursively.
+		 */
+		nc_rebuild(old_head, new_head, n->l, now);
+	}
+
+	if (n->b > n->r->b) {
+		/*
+		 * If the trie extends via the right branch, follow it
+		 * recursively.
+		 */
+		nc_rebuild(old_head, new_head, n->r, now);
+	}
+
+	if ((now - n->seen) < CONF_NEG_CACHE) {
+		/*
+		 * We want to keep this node, so insert it into the new
+		 * trie.
+		 */
+		new = nc_insert(new_head, n->ip);
+		new->seen = n->seen;
+	} else if (OPT_DEBUG) {
+		log("Deleting negcache node for %lu added at %lu", n->ip,
+		    n->seen);
+	}
+
+	/* Safe to free() this node now. */
+	free(n);
+}
+
+/*
+ * Wrapper for recursive rebuild function.
+ */
+void negcache_rebuild(void)
+{
+	time_t now;
+	struct cnode *new_head;
+
+	now = time(NULL);
+	new_head = NULL;
+
+	nc_init(&new_head);
+	nc_rebuild(nc_head, new_head, NULL, now);
+	free(nc_head);
+	nc_head = new_head;
+}
---
+++ src/negcache.h
@@ -0,0 +1,16 @@
+#ifndef NEGCACHE_H
+#define NEGCACHE_H
+
+struct cnode {
+	unsigned long ip;	/* IP address, network byte order. */
+	time_t seen;		/* When it was last seen. */
+	unsigned int b;		/* Index of bit to be tested. */
+	struct cnode *l;	/* Node to the left. */
+	struct cnode *r;	/* Node to the right. */
+};
+
+extern void nc_init(struct cnode **head);
+extern struct cnode *check_neg_cache(const unsigned long ip);
+extern void negcache_insert(const char *ipstr);
+
+#endif
