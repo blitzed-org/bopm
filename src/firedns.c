314513f7f7a954ac2d53a5fcfda064bfd4124b32
andy <andy@blitzed.org>
2003-03-07 00:43:32 +0000

    Add checks for size of int/long.  Work out what type to use for a 32-bit int.  Regenerate autotools.

--- src/firedns.c
+++ src/firedns.c
@@ -87,7 +87,7 @@ struct s_rr_middle
 {
    unsigned short type;
    unsigned short class;
-   unsigned int ttl; /* XXX - firedns depends on this being 4 bytes */
+   uint32 ttl; /* XXX - firedns depends on this being 4 bytes */
    unsigned short rdlength;
 };
 
