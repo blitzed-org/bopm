a32f7a0774503185df52a1b4e1e0380db26ac485
dg <dg@blitzed.org>
2004-08-23 22:43:21 +0000

    Of course it would help if I could type

--- src/scan.c
+++ src/scan.c
@@ -424,12 +424,12 @@ void scan_connect(char **user, char *msg)
                      break;
                   case OPM_ERR_BADADDR:
                      log_printf("OPM -> Bad address %s [%s].",
-                           (ss->manual_target->name ? ss->manual_target_name :
+                           (ss->manual_target ? ss->manual_target->name :
                            "(unknown)"), ss->ip);
                      break;
                   default:
                      log_printf("OPM -> Unknown error %s [%s].",
-                           (ss->manual_target->name ? ss->manual_target_name :
+                           (ss->manual_target ? ss->manual_target->name :
                            "(unknown)"), ss->ip);
                      break;
                }
